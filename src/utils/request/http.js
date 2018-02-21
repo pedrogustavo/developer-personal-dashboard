cd22622a0622b1dfd61090878d7d261a41564099
Pedro Gustavo <pedro.gustavo.pg@hotmail.com>
2018-02-21 18:20:06 +0000

    Change headers of the axios config

--- src/utils/request/http.js
+++ src/utils/request/http.js
@@ -1,8 +1,5 @@
 import axios from 'axios'
 
 export const http = axios.create({
-  baseURL: 'https://api.github.com/',
-  headers: {
-    'crossDomain': true
-  }
+  baseURL: 'https://api.github.com/'
 })
