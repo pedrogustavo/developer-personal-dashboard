513cc96b083b0eef7d213c947357d57ca27fbe02
Pedro Gustavo <pedro.gustavo.pg@hotmail.com>
2018-02-21 18:23:05 +0000

    Create list respositories component

---
+++ src/components/listRepositories/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './listRepositories.css'
+
+// get('users/pedrogustavo/repos')
+//   .then((response) => {
+//     console.log(response)
+//   })
+
+const ListRepositories = () => (
+  <div className="box-repositories">
+    <h3 className="repo-title">Seus Repositórios</h3>
+    <ul className="repo-list">
+      <li className="repo-list-item">
+        <a className="repo-list-item-link" target="_brank" href="/stone-payments/onestap-developer-webapp">onestap-developer-webapp</a>
+      </li>
+    </ul>
+  </div>
+)
+
+
+export default ListRepositories
