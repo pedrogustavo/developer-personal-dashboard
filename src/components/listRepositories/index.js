eaea79816bf6634bd14e2a63726ab2933a7ec237
Pedro Gustavo <pedro.gustavo.pg@hotmail.com>
2018-02-21 18:48:14 +0000

    Get data of repositories

--- src/components/listRepositories/index.js
+++ src/components/listRepositories/index.js
@@ -1,21 +1,46 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { get } from '../../utils'
 import './listRepositories.css'
 
-// get('users/pedrogustavo/repos')
-//   .then((response) => {
-//     console.log(response)
-//   })
 
-const ListRepositories = () => (
-  <div className="box-repositories">
-    <h3 className="repo-title">Seus Repositórios</h3>
-    <ul className="repo-list">
-      <li className="repo-list-item">
-        <a className="repo-list-item-link" target="_brank" href="/stone-payments/onestap-developer-webapp">onestap-developer-webapp</a>
-      </li>
-    </ul>
-  </div>
-)
+class ListRepositories extends Component {
+
+  constructor () {
+    super()
+    this.state = {
+      repositories: []
+    }
+  }
+
+  componentWillMount () {
+    get('users/pedrogustavo/repos')
+      .then((response) => {
+        this.setState({
+          repositories: response.data
+        })
+      })
+  }
+  
+  render () {
+    return (
+      <div className="box-repositories">
+        <h3 className="repo-title">Seus Repositórios</h3>
+        <ul className="repo-list">
+          {this.state.repositories.map((repo, index) => (
+            <li className="repo-list-item" key={index}>
+              <a
+                className={`repo-list-item-link ${repo.private ? 'private' : 'public'}`}
+                target="_brank"
+                href={repo.html_url}>
+                  {repo.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
+}
 
 
 export default ListRepositories
