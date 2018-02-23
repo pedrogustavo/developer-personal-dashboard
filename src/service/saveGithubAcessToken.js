cf998aefe418065575b3106c1c20d9ae3697ab11
Pedro Gustavo <pedro.gustavo.pg@hotmail.com>
2018-02-23 16:35:59 +0000

    Updates

--- src/service/saveGithubAcessToken.js
+++
@@ -1,6 +0,0 @@
-import { localStorage } from '../utils'
-
-export const saveGithubAcessToken = () => {
-  let accessToken = prompt('Insira seu token de acesso do Github')
-  localStorage.add('githubAcesstoken', accessToken)
-}
